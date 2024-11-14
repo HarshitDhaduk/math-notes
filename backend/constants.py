f66b160e8fbe0784b1b34c7b64e49f75d82468c2
HarshitDhaduk <harshitdhaduk5831@gmail.com>
2024-11-14 23:18:26 +0530

    update main.py

--- backend/constants.py
+++ backend/constants.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
+SERVER_URL = 'math-notes-production.up.railway.app'
 PORT = '8900'
 ENV = 'dev'
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
